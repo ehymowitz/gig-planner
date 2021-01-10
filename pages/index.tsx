576db8eab6e90c81e53dce74dfcedc91b7ed2ca8
Elie Hymowitz <ehymowitz@gmail.com>
2021-01-10 12:19:58 -0500

    Chakra UI init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import Head from 'next/head'
+import { Box } from "@chakra-ui/react"
+
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Website Template</title>
+        <title>Gig Planner</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Box bg="gray.400" w="100%" h="100vh" p={4} color="white">
+        Menu on the left, content on the right
+      </Box>
 
     </div>
   )
